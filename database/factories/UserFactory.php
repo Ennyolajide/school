df57e42b45b148550d1323e4c8eddad0e7284816
Eniseyin Olajide <eniseyinolajde@gmail.com>
2020-01-23 20:11:31 +0100

    added authentication scafold

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,19 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'other_name' => $faker->firstName,
+        'dob' => $faker->date,
+        'phone' => '+234'.$faker->numberBetween(7,9).$faker->numberBetween(0,1).$faker->numberBetween(10001009,99999900),
+        'address' => $faker->address,
+        'matric_number' => '20'.$faker->numberBetween(10,19).$faker->numberBetween(1000,5000),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
 });
+
+
+
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('other_name')->nullable();
+            $table->date('dob');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('matric_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
